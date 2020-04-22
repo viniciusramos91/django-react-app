a0b08cfad768938de592f9947909826843f3cdd0
Vinicius Ramos <vinicius.ramos91@gmail.com>
2020-04-22 00:04:51 -0300

    Adiciona dependência django-filters

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'website',
     'core',
